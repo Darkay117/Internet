b7a73da9bfe1764723703f27344130ac18f63091
Canaima <68438095+Darkay117@users.no-reply.github.com>
2020-11-29 13:41:31 -0430

    version subida a repl

---
+++ scripts/Javap.js
@@ -0,0 +1,14 @@
+// < menor que
+// > mayor que
+// >= mayor o igual
+// <= menor o igual
+// == igual que
+// != distinto de 
+// if (condicion && otra condicion) o (condicion || si no se puede ejecutar la otra se ejecuta mediante esta condicion)
+let edad = prompt("Escribe tu edad");
+
+if (edad >= 18){
+    document.write("¡Bienvenido de nuevo!")
+} else if(edad < 18) {
+    document.write("No eres bienvenido, vuevle cuando tengas edad")
+}
